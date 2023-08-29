3b9d359cd03d56ed938d13fe51086ada53cdcfa7
Matt <matt.costuros@gmail.com>
2023-08-29 07:55:44 -0400

    add create model

--- src/Common/Entities/BikeRoute.cs
+++ src/Common/Entities/BikeRoute.cs
@@ -2,9 +2,16 @@ namespace CCC.Entities;
 
 public class BikeRoute
 {
-    public Guid Id { get; set;} = Guid.NewGuid();
+    public Guid Id { get; set;}
     public string Name {get;set;} = string.Empty;
     public string Description {get;set;} =  string.Empty;
     public List<CueEntry> Cues { get; set; } = new();
     public double Distance {get; set;}
+}
+
+public class BikeRouteCreateModel
+{
+    public string Name {get;set; } = string.Empty;
+    public string Description {get;set; } = string.Empty;
+    public double Distance { get;set;}
 }
