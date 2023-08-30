1c61e95cf7a0dae9c0b3e8eeb7aa61b997287b5c
Matt <matt.costuros@gmail.com>
2023-08-30 08:07:34 -0400

    1 more update model

---
+++ src/Common/Entities/RideEventUpdateModel.cs
@@ -0,0 +1,10 @@
+namespace CCC.Entities;
+
+public class RideEventUpdateModel
+{
+    public string? Name { get; set; } 
+    public string? Description { get; set; } 
+    public DateTime? StartTime { get; set; }
+    public string? Location { get; set; }
+
+}
