28485fa65c165f0d077f2a26c4d4962196d88d56
Matt <matt.costuros@gmail.com>
2023-08-30 18:21:00 -0400

    create model uses dateTime

--- src/Common/Entities/RideEventCreateModel.cs
+++ src/Common/Entities/RideEventCreateModel.cs
@@ -4,7 +4,7 @@ public class RideEventCreateModel
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public DateTimeOffset StartTime { get; set; }
+    public DateTime StartTime { get; set; }
     public string Location { get; set; } = string.Empty;
     public List<Guid> Rides { get; set; } = new();
 
