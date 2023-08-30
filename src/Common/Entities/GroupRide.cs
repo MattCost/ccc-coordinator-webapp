42164fa4de094536dd31c6fa728ebe4203aeeaef
Matt <matt.costuros@gmail.com>
2023-08-30 07:55:19 -0400

    make model more generic

--- src/Common/Entities/GroupRide.cs
+++ src/Common/Entities/GroupRide.cs
@@ -1,49 +1,17 @@
-namespace CCC.Entities;
+using System.Security;
 
+namespace CCC.Entities;
+public class CoordinatorEntry
+{
+    public List<string> CoordinatorIds { get; set;} = new();    //guid? email?
+    public int RequiredCount { get; set; }
+    public bool RequiredCountMet => CoordinatorIds.Count >= RequiredCount;
+}
 public class GroupRide
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; }
     public RideType RideType { get; set; }
     public Guid BikeRouteId { get; set; }
     public Guid RideEventId { get; set; }
-    public Dictionary<CoordinatorRole, List<Guid>> Coordinators { get; set; } = new Dictionary<CoordinatorRole, List<Guid>>
-    {
-        [CoordinatorRole.Lead] = new(),
-        [CoordinatorRole.Mid] = new(),
-        [CoordinatorRole.Sweep] = new()
-    };
-
-    public int LeadRequiredCount
-    {
-        get
-        {
-            return (RideType == RideType.A30) ? 2 : 1;
-        }
-    }
-    public bool LeadCountMet { get { return Coordinators[CoordinatorRole.Lead].Count >= LeadRequiredCount; } }
-
-    public int MidRequiredCount
-    {
-        get
-        {
-            switch (RideType)
-            {
-                case RideType.A30: return 0;
-                case RideType.C: return 1;
-                default: return 2;
-            }
-        }
-    }
-    public bool MidCountMet { get { return Coordinators[CoordinatorRole.Mid].Count >= MidRequiredCount; } }
-
-    public int SweepRequiredCount
-    {
-        get
-        {
-            return (RideType == RideType.A30) ? 2 : 1;
-        }
-    }
-
-    public bool SweepCountMet { get { return Coordinators[CoordinatorRole.Sweep].Count >= SweepRequiredCount; } }
-
+    public Dictionary<CoordinatorRole, CoordinatorEntry> Coordinators {get;set;} = new();   
 }
