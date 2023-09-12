6f2664b882274d7752e3d51d837a3413516e8d2c
Matt <matt.costuros@gmail.com>
2023-09-11 22:14:55 -0400

    print preview is smaller

--- src/Common/Entities/CueEntry.cs
+++ src/Common/Entities/CueEntry.cs
@@ -1,21 +1,23 @@
 namespace CCC.Entities;
 
-public enum CueOperation
-{
-    StartRoute = 0,
-    Left = 1,
-    Right = 2,
-    Cross = 3,
-    Straight = 4,
-    LeftAt = 5,
-    RightAt = 6,
-    SlightLeft = 7,
-    SlightRight = 8,
-    LeftCircle = 9,
-    RightCircle = 10,
-    StraightCircle = 11,
-    EndRoute = 12
-}
+using CCC.Enums;
+
+// public enum CueOperation
+// {
+//     StartRoute = 0,
+//     Left = 1,
+//     Right = 2,
+//     Cross = 3,
+//     Straight = 4,
+//     LeftAt = 5,
+//     RightAt = 6,
+//     SlightLeft = 7,
+//     SlightRight = 8,
+//     LeftCircle = 9,
+//     RightCircle = 10,
+//     StraightCircle = 11,
+//     EndRoute = 12
+// }
 
 public class CueEntry
 {
---
+++ src/Common/Enums/CueOperation.cs
@@ -0,0 +1,77 @@
+using System.Reflection;
+
+namespace CCC.Enums;
+
+public enum CueOperation
+{
+    [CueOperationPrintAttribute("a")]
+    StartRoute = 0,
+
+    [CueOperationPrintAttribute("L")]
+    Left = 1,
+
+    [CueOperationPrintAttribute("R")]
+    Right = 2,
+
+    [CueOperationPrintAttribute("X")]
+    Cross = 3,
+
+    [CueOperationPrintAttribute("S")]
+    Straight = 4,
+
+    [CueOperationPrintAttribute("L@")]
+    LeftAt = 5,
+
+    [CueOperationPrintAttribute("R@")]
+    RightAt = 6,
+
+    [CueOperationPrintAttribute("sL")]
+    SlightLeft = 7,
+
+    [CueOperationPrintAttribute("sR")]
+    SlightRight = 8,
+
+    [CueOperationPrintAttribute("(L)")]
+    LeftCircle = 9,
+
+    [CueOperationPrintAttribute("(R)")]
+    RightCircle = 10,
+
+    [CueOperationPrintAttribute("(S)")]
+    StraightCircle = 11,
+    [CueOperationPrintAttribute("z")]
+    EndRoute = 12
+}
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class CueOperationPrintAttribute : System.Attribute
+{
+    public string Value;
+
+    public CueOperationPrintAttribute(string value)
+    {
+        Value = value;
+
+    }
+}
+
+
+public static class CueOperationExtensions
+{
+    public static string GetPrintAttribute(this CueOperation operation)
+    {
+        Type typeOfEnum = operation.GetType(); //this will be typeof( MyEnum )
+
+        //here is the problem, GetField takes a string
+        // the .ToString() on enums is very slow
+        FieldInfo fi = typeOfEnum.GetField(operation.ToString()) ?? throw new Exception();
+
+        //get the attribute from the field
+        var printAttribute = fi.GetCustomAttributes(typeof(CueOperationPrintAttribute), false).FirstOrDefault()
+            as CueOperationPrintAttribute ?? throw new Exception();
+
+        return printAttribute.Value;
+
+
+    }
+}
