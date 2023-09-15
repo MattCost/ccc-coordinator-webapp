e84a596d2411c3a0ed1fcaa059ffe7ec460806a7
Matt <matt.costuros@gmail.com>
2023-09-15 07:22:44 -0400

    adding enum for entity type

---
+++ src/Common/Enums/EntityTypes.cs
@@ -0,0 +1,9 @@
+namespace CCC.Enums;
+
+public static class EntityTypes
+{
+    public const string BikeRoute = nameof(BikeRoute);
+    public const string GroupRide = nameof(GroupRide);
+    public const string RideEvent = nameof(RideEvent);
+
+}
