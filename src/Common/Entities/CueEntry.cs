2bcf4e856eb8a874f6aaca03b9e790d21666c7b2
Matt <matt.costuros@gmail.com>
2024-03-02 12:24:54 -0500

    Use String Converter

--- src/Common/Entities/CueEntry.cs
+++ src/Common/Entities/CueEntry.cs
@@ -1,26 +1,12 @@
 namespace CCC.Entities;
 
+using System.Text.Json.Serialization;
 using CCC.Enums;
 
-// public enum CueOperation
-// {
-//     StartRoute = 0,
-//     Left = 1,
-//     Right = 2,
-//     Cross = 3,
-//     Straight = 4,
-//     LeftAt = 5,
-//     RightAt = 6,
-//     SlightLeft = 7,
-//     SlightRight = 8,
-//     LeftCircle = 9,
-//     RightCircle = 10,
-//     StraightCircle = 11,
-//     EndRoute = 12
-// }
 
 public class CueEntry
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public CueOperation Operation { get; set; }
     public string StreetName { get; set; } = string.Empty;
     public string Notes {get;set; } = string.Empty;
