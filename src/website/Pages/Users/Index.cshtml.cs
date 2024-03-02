a5fd1e73a446608086a22c314a9729ce39ad2884
Matt <matt.costuros@gmail.com>
2024-03-02 18:53:19 -0500

    User Mgmt placeholder

--- src/website/Pages/Users/Index.cshtml.cs
+++ src/website/Pages/Users/Index.cshtml.cs
@@ -1,5 +1,6 @@
 
 using CCC.Entities;
+using CCC.Authorization;
 using CCC.website.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,8 @@ namespace CCC.website.Pages.Users
     [Authorize(Policy = CCC.Authorization.Enums.CoordinatorAdminPolicy)]
     public class IndexPageModel : PageModelBase
     {
+        public bool ShowAdminSection => User.IsAdmin();
+        public bool ShowCoordinatorAdminSection => User.IsCoordinatorAdmin();
         public IndexPageModel(ILogger<IndexPageModel> logger, IDownstreamApi api) : base(logger, api)
         {
         }
