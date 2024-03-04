c47c6114d87ef6644ebe7258e7e7732c9ef7b0d4
Matt <matt.costuros@gmail.com>
2024-03-03 20:03:56 -0500

    user mgmt

--- src/website/Pages/Users/Index.cshtml.cs
+++ src/website/Pages/Users/Index.cshtml.cs
@@ -37,5 +37,42 @@ namespace CCC.website.Pages.Users
                 return new JsonResult(new {});
             }
         }
+
+        public async Task<JsonResult> OnPostAssignCoordinatorAsync(string userId)
+        {
+            Logger.LogTrace("Entering OnPostAssignCoordinatorAsync User Id", userId);
+            try
+            {
+                await API.PatchForUserAsync("API", string.Empty, options => {
+                    options.RelativePath = $"Users/{userId}/coordinator";
+                });
+                Logger.LogTrace("Returning ok");
+                return new JsonResult(new {message = "ok"});
+            }
+            catch(Exception ex)
+            {
+                Logger.LogError(ex, "Exception trying to Assign Coordinator to user Id {UserId}", userId);
+                return new JsonResult(new {message = "problem"});
+
+            }
+        }
+        public async Task<JsonResult> OnPostRemoveCoordinatorAsync(string userId)
+        {
+            Logger.LogTrace("Entering OnPostRemoveCoordinator User Id", userId);
+            try
+            {
+                await API.DeleteForUserAsync("API", string.Empty, options => {
+                    options.RelativePath = $"Users/{userId}/coordinator";
+                });
+                Logger.LogTrace("Returning ok");
+                return new JsonResult(new {message = "ok"});
+            }
+            catch(Exception ex)
+            {
+                Logger.LogError(ex, "Exception trying to Remove Coordinator from user Id {UserId}", userId);
+                return new JsonResult(new {message = "problem"});
+
+            }
+        }        
     }
 }
