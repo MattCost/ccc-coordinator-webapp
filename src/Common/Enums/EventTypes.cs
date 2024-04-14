b175cb2faa02645c641f17406f10d62896d33d25
Matt <matt.costuros@gmail.com>
2024-04-14 17:37:28 -0400

    adding event type

--- src/Common/Enums/EventTypes.cs
+++ src/Common/Enums/EventTypes.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace CCC.Enums;
@@ -5,8 +6,16 @@ namespace CCC.Enums;
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum EventTypes
 {
+    [Display(Name = "Unknown")]
     Unknown = 0,
+
+    [Display(Name = "Road Ride")]
     Road = 1,
+
+    [Display(Name = "GravGrav")]
+    
     Gravel = 2,
+
+    [Display(Name = "Gnar")]
     MTB = 3,
 }
