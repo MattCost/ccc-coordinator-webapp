78766eedeb7cbcdebd4bd813e0a3f06674dfa7f5
Matt <matt.costuros@gmail.com>
2024-05-09 09:17:39 -0400

    update app service version

--- infrastructure/app-service.tf
+++ infrastructure/app-service.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_web_app" "api" {
     always_on = true
 
     application_stack {
-      dotnet_version = "7.0"
+      dotnet_version = "8.0"
     }
 
   }
@@ -72,7 +72,7 @@ resource "azurerm_linux_web_app" "website" {
     always_on = true
 
     application_stack {
-      dotnet_version = "7.0"
+      dotnet_version = "8.0"
     }
   }
 
