d9ab2189a43084a15a1584d1a502462249750f92
Matt <matt.costuros@gmail.com>
2024-05-09 21:58:29 -0400

    show next ride deets

--- src/website/Pages/Index.cshtml.cs
+++ src/website/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using CCC.Entities;
+﻿using CCC.Authorization;
+using CCC.Entities;
+using CCC.ViewModels;
 using CCC.website.Models;
 using Microsoft.Identity.Abstractions;
 
@@ -6,6 +8,7 @@ namespace website.Pages;
 
 public class IndexModel : PageModelBase
 {
+    public RideEventViewModel? NextRideEventVM {get;set;} = null;
     public IndexModel(ILogger<IndexModel> logger, IDownstreamApi api) : base(logger, api)
     {
     }
@@ -19,7 +22,56 @@ public class IndexModel : PageModelBase
                 options.RelativePath = "RideEvents";
             });
             Logger.LogDebug("Result from API {Result}", result);
-            ViewData["RideEvents"] = result ?? new List<RideEvent>();
+            var allRides = result ?? new List<RideEvent>();
+            ViewData["RideEvents"] = allRides;
+            var nextRides = allRides.Where(ride => ride.StartTime >= DateTimeOffset.UtcNow ).OrderBy(ride => ride.StartTime); 
+            if(nextRides.Any())
+            {
+                var nextRideEventId = nextRides.First().Id;
+                var rideEventResult = await API.GetForUserAsync<RideEventViewModel>("API", options =>
+                {       
+                    options.RelativePath = $"ViewModels/RideEvents/{nextRideEventId}";
+                });
+                Logger.LogDebug("Result from API {Result}", rideEventResult);
+                NextRideEventVM = rideEventResult;
+                if(NextRideEventVM != null)
+                {
+                    var coordinatorSignedUpRide = NextRideEventVM.GroupRides.Where(ride => ride.Coordinators.Values.SelectMany( x => x.CoordinatorIds).Contains(User.NameIdentifier()));
+                    var supportSignedUpRole = NextRideEventVM.RideEvent.SupportPersonnel.Where(kvp => kvp.Value.CoordinatorIds.Contains(User.NameIdentifier()));
+                    if(coordinatorSignedUpRide.Any())
+                    {
+                        Logger.LogDebug("User is signed up for a role");
+                        var rideType = coordinatorSignedUpRide.First().RideType;
+                        var position = coordinatorSignedUpRide.First().Coordinators.Where(kvp => kvp.Value.CoordinatorIds.Contains(User.NameIdentifier())).First().Key;
+                        var routeId = coordinatorSignedUpRide.First().BikeRouteId;
+                        ViewData["RideType"] = rideType;
+                        ViewData["Position"] = position;
+                        ViewData["RouteId"] = routeId;
+                        ViewData["RouteName"] = NextRideEventVM.BikeRoutes[routeId].Name;
+                    }
+                    
+
+                    if(supportSignedUpRole.Any())
+                    {
+                        Logger.LogDebug("user is signed up for support");
+                        ViewData["SupportRole"] = supportSignedUpRole.First().Key;
+                    }
+
+                    if(!coordinatorSignedUpRide.Any() && !supportSignedUpRole.Any())
+                    {
+                        ViewData["Message"] = "Go Signup!";
+                    }
+
+
+                    // var coordinatorSignedUcp = NextRideEventVM.GroupRides.Select(ride => ride.Coordinators.Values.ToList()).SelectMany(x => x).Where(entry => entry.CoordinatorIds.Contains(User.NameIdentifier()));
+                    // var coordinatorSignedUp = NextRideEventVM.GroupRides.Select(ride => ride.Coordinators.Values.ToList()).SelectMany(x => x).Where(entry => entry.CoordinatorIds.Contains(User.NameIdentifier())).Any();
+                    // var supportSignedUp = NextRideEventVM.RideEvent.SupportPersonnel.Select( entry => entry.Value).ToList().Where(entry => entry.CoordinatorIds.Contains(User.NameIdentifier())).Any();
+                    // Logger.LogDebug("CoordinatorSignedUp: {coordinatorSignedUp}", coordinatorSignedUp);
+                    // var IsSignedUp = coordinatorSignedUp | supportSignedUp;
+                }
+
+
+            }
         }
         catch (Exception ex)
         {
@@ -27,5 +79,6 @@ public class IndexModel : PageModelBase
             CurrentPageAction = "OnGetAsync";
             CurrentPageErrorMessage = ex.Message;
         }
+
     }
 }
